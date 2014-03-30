77614213ecaecea0cf3ea2df4692596504fda660
Kelong Cong <kelongcong@gmail.com>
2014-03-30 23:31:53 +0100

    adjusted the physical constants to suit the application

--- config/config.hpp
+++ config/config.hpp
@@ -1,11 +1,12 @@
 // global configuration file
+//
 #define MAX_PATH_LEN 256
-#define LOOP_DELAY 30
-#define V_MAX 220.
-#define V_0 200.
-#define G 400.
+#define LOOP_DELAY 30 // in ms
 
-//Screen dimension constants
+#define V_MAX 700. // terminal velocity
+#define V_0 450. // initial velocity
+#define G 1200. // acceleration
+
 #define SCREEN_WIDTH 640
 #define SCREEN_HEIGHT 480
 
