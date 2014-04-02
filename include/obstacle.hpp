871077f936ba2c7da643aa3f1724f00b7340a322
stephenyao <stephenryao@gmail.com>
2014-04-03 00:22:37 +1100

    Implemented obstacle class

--- include/obstacle.hpp
+++ include/obstacle.hpp
@@ -1,15 +1,32 @@
 #ifndef _OBSTACLES_HPP_
 #define _OBSTACLES_HPP_
 
-#include "texture.hpp"
-class Obstacle : public Texture
+#include "SDL2/SDL.h"
+
+// TODO this class doesn't subclass from Texture. But it should subclass something in common with player, maybe create a entity class to contain all the posx,posy,w,h in entity and keep the renderering code in Texture.
+// and have player multiple inherit from Texture and Entity.
+
+class Obstacle
 {
 	public:
-		int dummy;
-		Obstacle();
-		~Obstacle();
-	private:
-		int dummy2;
-};
+		int getPosX();
+		int getPosY();
+		int getWidth();
+		int getHeight();
 
+		void setPosX(int x);
+		void setPosY(int y);
+		void setWidth(int w);
+		void setHeight(int h); 
+
+		void render(SDL_Renderer *renderer);
+		Obstacle(int x, int y, int w, int h);
+		~Obstacle();
+
+	private:
+		int posX;		
+		int posY;
+		int width;
+		int height;
+};
 #endif
--- src/obstacle.cpp
+++ src/obstacle.cpp
@@ -1,6 +1,69 @@
 
 #include "obstacle.hpp"
 
-Obstacle::Obstacle() : Texture() {}
+// Obstacle class
+// TODO: make a new class called entity class, and have player and obstacle and background inherit from that.
 
-Obstacle::~Obstacle() {}
+void Obstacle::setPosX(int x)
+{
+	posX = x;
+}
+
+void Obstacle::setPosY(int y)
+{
+	posY = y;
+}
+
+void Obstacle::setWidth(int w)
+{
+	width = w;
+}
+
+void Obstacle::setHeight(int h)
+{
+	height = h;
+}
+
+int Obstacle::getPosX()
+{
+	return posX;
+}
+
+int Obstacle::getPosY()
+{
+	return posY;
+}
+
+int Obstacle::getWidth()
+{
+	return width;
+}
+
+int Obstacle::getHeight()
+{
+	return height;
+}
+
+void Obstacle::render(SDL_Renderer *renderer)
+{
+	SDL_Rect obstacleRect = {posX,posY,width,height};
+	SDL_SetRenderDrawColor(renderer, 255,0,0,255);
+	SDL_RenderDrawRect(renderer, &obstacleRect);	
+	SDL_RenderFillRect(renderer, &obstacleRect);
+}
+
+Obstacle::Obstacle(int x, int y, int w, int h)
+{
+	posX = x;
+	posY = y;
+	width = w;
+	height = h;
+}
+
+Obstacle::~Obstacle()
+{
+	posX = 0;
+	posY = 0;
+	width = 0;
+	height = 0;
+}
